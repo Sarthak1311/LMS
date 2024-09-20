8e02bcd5eb09942f137605323bec82d21bbd3e11
sarthaktyagi1704@gmail.com <sarthaktyagi1704@gmail.com>
2024-09-20 17:00:10 +0530

    minor alteration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.LibManSys.Patron import Patron
 import datetime
 a =Patron()
-a.patronInfo("sarthak tyagi")
+# a.add("pulkit tyagi","harry potter",datetime.datetime.now().strftime('%Y-%m-%d'))
+# a.patronInfo("sarthak tyagi")
+a.listAllPatron()
